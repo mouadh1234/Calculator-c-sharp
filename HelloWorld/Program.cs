5118e6923522d513cb9976abe8a7e9cec5b3274c
mouadh <mouadh1234@gmail.com>
2023-11-14 19:41:38 +0100

    add switch case

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, to calculator app!");
 int num1=0;
 int num2=0;
 Console.WriteLine("console calculator in c#\r");
